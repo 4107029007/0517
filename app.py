aa3a1b1e24793c5fa2c09f4d3888dba7e7282eaa
4107029007 <syringanorah@gmail.com>
2022-05-17 11:59:12 +0800

    git

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+from distutils.log import debug
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5050, debug=True)
