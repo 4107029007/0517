d33fcd0ec581f70a94476626644e1db05eedc89a
4107029007 <syringanorah@gmail.com>
2022-05-17 12:15:19 +0800

    v3

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/<name>')
 @app.route('/')
-def index(name=''):
-    return render_template('index.html', name=name)
+def index():
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(port=5050, debug=True)
