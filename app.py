9de97e891c12733ec883dd814a0f1e71420e5546
4107029007 <syringanorah@gmail.com>
2022-05-17 12:04:15 +0800

    v4

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/<name>')
 @app.route('/')
-def index():
-    return app.send_static_file('index.html')
+def index(name=''):
+    return reder_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run(port=5050, debug=True)
